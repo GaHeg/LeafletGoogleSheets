4ab1afa6e0c78819af66573c2b1f524fa7047913
GaHeg <gaute.heggen@gmail.com>
2026-09-13 22:25:09 +0200

    Change Google Sheets URL to specific gid
    Updated Google Sheets URL to include specific gid parameter.

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_VvgcdSVErp-oQN3gmxPd2srqVU-1GyuwbB4huirLoM/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_VvgcdSVErp-oQN3gmxPd2srqVU-1GyuwbB4huirLoM/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
